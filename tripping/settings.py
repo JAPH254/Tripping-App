f00d0c85b2e78e31b8ab8c69921dd263418255e8
JAPH254 <omanyujapheth@gmail.com>
2025-03-21 22:30:46 +0300

    feat: configure CORS settings for local development

--- tripping/settings.py
+++ tripping/settings.py
@@ -40,10 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'trips',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
