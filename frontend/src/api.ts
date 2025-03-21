e4ee9c66077433b3dbd5199c6d9e024f86a80c8d
JAPH254 <omanyujapheth@gmail.com>
2025-03-21 22:26:24 +0300

    feat: add API service for trip management with axios

---
+++ frontend/src/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+
+const API_URL = "http://127.0.0.1:8000/api/";
+
+export interface Trip {
+  id?: number;
+  current_location: string;
+  pickup_location: string;
+  dropoff_location: string;
+  cycle_hours_used: number;
+}
+
+export const getTrips = async () => {
+  const response = await axios.get<Trip[]>(`${API_URL}trips/`);
+  console.log(response)
+  return response.data;
+};
+
+export const createTrip = async (tripData: Trip) => {
+  const response = await axios.post<Trip>(`${API_URL}trips/`, tripData);
+  return response.data;
+};
