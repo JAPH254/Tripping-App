4bec645555245b49848bc8d46eccdd94e91cc2bf
JAPH254 <omanyujapheth@gmail.com>
2025-03-25 13:20:12 +0300

    feat: add routes for getting routes and managing ELD logs

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TripListCreateView
+from .views import TripListCreateView, get_route, ELDLogViewSet
 
 urlpatterns = [
     path('trips/', TripListCreateView.as_view(), name='trip-list-create'),
+    path('api/route/', get_route, name="get_route"),
+    path('eld_logs/', ELDLogViewSet.as_view({'get': 'list', 'post': 'create'}), name='eldlog-list-create')
 ]
