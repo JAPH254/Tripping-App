57683a54f2dda8183ea1c5aab3da0745510a443e
JAPH254 <omanyujapheth@gmail.com>
2025-03-25 13:19:58 +0300

    feat: add ELDLogSerializer to handle serialization of ELDLog model

--- trips/serializers.py
+++ trips/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from .models import Trip
+from .models import Trip, ELDLog
 
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
+        fields = '__all__'
+class ELDLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ELDLog
         fields = '__all__'
